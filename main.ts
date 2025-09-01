2da01aa12520cb0c72584adf2815bb580574a893
CBurg-College <cburgmicro@outlook.com>
2025-09-01 18:00:26 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,22 @@
+/*
+The code below is a refactoring of:
+- the ElecFreaks 'pxt-PlanetX' library:
+  https://github.com/elecfreaks/pxt-PlanetX/blob/master/basic.ts
+MIT-license.
+*/
+
+namespace PxSwitch {
+
+    let RJPORT = 0
+
+    export function setPort(port: RJPort) {
+        RJPORT = port
+    }
+
+    export function read(): Switch {
+        let pin = Nezha.digitalPin(RJPORT, RJLine.LA)
+        pins.setPull(pin, PinPullMode.PullUp)
+        let sensor = pins.digitalReadPin(pin)
+        return (sensor ? Switch.Pressed : Switch.Released)
+    }
+}
